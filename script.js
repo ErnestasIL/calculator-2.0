564a1d319c1a3b3e0d4112a2b496cae5191ddd16
Ernestas Ilgevicius <ernestas963@yahoo.com>
2025-01-08 19:11:14 +0200

    created style and added DOM elements

---
+++ script.js
@@ -0,0 +1,5 @@
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const clearButton = document.querySelector(".clear");
+const equalButton = document.querySelector(".equal");
+const dotButton = document.querySelector(".dot");
